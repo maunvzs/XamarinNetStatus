f6eea17552df8916861fb55b677b08f17c9f494a
Mauricio <mauricio.nvz@gmail.com>
2017-05-05 17:41:43 -0500

    first commit

---
+++ NetStatus/NetStatus/NoNetworkPage.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace NetStatus
+{
+	public class NoNetworkPage : ContentPage
+	{
+		public NoNetworkPage ()
+		{
+		    BackgroundColor = Color.FromRgb(0xf0, 0xf0, 0xf0);
+		    Content = new Label()
+		    {
+		        Text = "No Network Connection Available",
+		        HorizontalOptions = LayoutOptions.Center,
+		        VerticalOptions = LayoutOptions.Center,
+		        TextColor = Color.FromRgb(0x40, 0x40, 0x40),
+		    };
+        }
+	}
+}
